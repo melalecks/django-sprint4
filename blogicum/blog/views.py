c0763c9af90ff02704ebf4382c3896f2b1f80385
Aleksandr Melikhov <melalecks@yandex.ru>
2025-08-21 01:24:22 +0300

    3.1

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count
-from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 from django.utils import timezone
